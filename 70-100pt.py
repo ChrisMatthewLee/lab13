0eef728db23f60eb9f9b42c0a8af55844d77dbc7
ChrisMatthewLee <s571353@student.roundrockisd.org>
2014-11-06 13:31:26 -0600

    70pt

--- 70-100pt.py
+++ 70-100pt.py
@@ -24,11 +24,34 @@ class MyApp:
        	    self.myParent = parent  
        	    self.myContainer1 = Frame(parent)
        	    self.myContainer1.pack()
+       	    
+       	    #up button
        	    self.up = Button(self.myContainer1)
        	    self.up.configure(text="up", background= "green")
-       	    self.up.grid(row=0,column=0)
+       	    self.up.grid(row=0,column=1)
        	    # Bind an event to the first button
        	    self.up.bind("<Button-1>", self.upClicked)
+       	    
+       	    #down button
+       	    self.down = Button(self.myContainer1)
+       	    self.down.configure(text="down", background= "pink")
+       	    self.down.grid(row=3,column=1)
+       	    # Bind an event to the first button
+       	    self.down.bind("<Button-1>", self.downClicked)
+       	    
+       	    #left button
+       	    self.left = Button(self.myContainer1)
+       	    self.left.configure(text="left", background= "blue")
+       	    self.left.grid(row=2,column=0)
+       	    # Bind an event to the first button
+       	    self.left.bind("<Button-1>", self.leftClicked)
+       	    
+       	    #right button
+       	    self.right = Button(self.myContainer1)
+       	    self.right.configure(text="right", background= "red")
+       	    self.right.grid(row=2,column=3)
+       	    # Bind an event to the first button
+       	    self.right.bind("<Button-1>", self.rightClicked)
        	    
        	    # No need to edit this - just includes the drawpad into our frame
        	    drawpad.pack(side=RIGHT)
@@ -43,10 +66,29 @@ class MyApp:
 	    # Uncomment this when you're ready to test out your animation!
 	    #drawpad.after(10,self.animate)
 		
+	#Up	
 	def upClicked(self, event):   
 	   global oval
 	   global player
 	   drawpad.move(player,0,-20)
+	   
+	#down  
+        def downClicked(self, event):   
+	   global oval
+	   global player
+	   drawpad.move(player,0,20)
+	   
+	#left   
+        def leftClicked(self, event):   
+	   global oval
+	   global player
+	   drawpad.move(player,-20,0)
+	   
+	#right   
+	def rightClicked(self, event):   
+	   global oval
+	   global player
+	   drawpad.move(player,20,0)
 		
 
 app = MyApp(root)
